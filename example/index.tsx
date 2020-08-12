2ea1edabfd33e2fe8bc9cc9151156370cb5fb6eb
Yash Gupta <yashguptaz@protonmail.com>
2020-08-12 10:56:50 +0530

    Major Fixes

--- example/index.tsx
+++ example/index.tsx
@@ -1,9 +1,8 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { DirWrapper, Folder, File } from '../.';
+import Thing from '../.';
 import styled from 'styled-components';
-import { BrowserRouter as Router } from 'react-router-dom';
 
 const Container = styled.main`
   max-width: 700px;
@@ -13,20 +12,9 @@ const Container = styled.main`
 
 const App = () => {
   return (
-    <Router>
-      <Container>
-        <DirWrapper>
-          <Folder path="/react">
-            <File />
-          </Folder>
-          <File />
-          <Folder path="/rust">
-            <Folder path="/rustlings"></Folder>
-            <Folder path="/rustbook"></Folder>
-          </Folder>
-        </DirWrapper>
-      </Container>
-    </Router>
+    <Container>
+      <Thing />
+    </Container>
   );
 };
 
